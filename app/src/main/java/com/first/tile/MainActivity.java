4f99d0b63aec0830b3028087073c6ee3731fd391
flenkeaustin <flenkeaustin@qq.com>
2019-06-11 00:02:08 +0800

    add icon toggle

--- app/src/main/java/com/first/tile/MainActivity.java
+++ app/src/main/java/com/first/tile/MainActivity.java
@@ -1,13 +1,15 @@
 package com.first.tile;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class MainActivity extends Activity {
-    private Switch mSwitch;
+    private Switch mSwitch,hSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +37,26 @@ public class MainActivity extends Activity {
                     }
                 }
             });
-        }
-
+        hSwitch=(Switch)findViewById(R.id.switch_hide);
+        hSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                        showLauncherIcon(false);
+                }else{
+                        showLauncherIcon(true);
+                }
+            }
+        });
     }
+    public void showLauncherIcon(boolean isShow){
+        PackageManager packageManager = this.getPackageManager();
+        int show = isShow? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        packageManager.setComponentEnabledSetting(getAliseComponentName(), show , PackageManager.DONT_KILL_APP);
+    }
+    private ComponentName getAliseComponentName(){
+        return new ComponentName(MainActivity.this, "com.first.tile.MainActivityAlias");
+        // 在AndroidManifest.xml中为MainActivity定义了一个别名为MainActivity-Alias的activity，是默认启动activity、是点击桌面图标后默认程序入口
+    }
+}
