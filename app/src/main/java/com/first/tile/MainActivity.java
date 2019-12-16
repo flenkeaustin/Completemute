361451e1abb167855b02d6be32a6160682fe17c9
sky5454 <sky5454@users.noreply.github.com>
2019-12-17 00:04:39 +0800

    Optimize code && add Features(LastVolume) && fixed a bug

--- app/src/main/java/com/first/tile/MainActivity.java
+++ app/src/main/java/com/first/tile/MainActivity.java
@@ -8,53 +8,44 @@ import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
+import com.first.tile.Methods.VolumeControler;
+
 public class MainActivity extends Activity {
-    private Switch mSwitch,hSwitch;
+    private Switch  mSwitch, hSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-            mSwitch = (Switch) findViewById(R.id.switch_all);
-            // 添加监听
-            mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        AudioManager audioManager=(AudioManager)getSystemService(AUDIO_SERVICE);
-                        audioManager.setStreamVolume(AudioManager.STREAM_ALARM,0,0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,0,0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_RING,0,0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION,0,0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,0,0);
-                    } else {
-                        AudioManager audioManager=(AudioManager)getSystemService(AUDIO_SERVICE);
-                        audioManager.setStreamVolume(AudioManager.STREAM_ALARM,audioManager.getStreamMaxVolume(AudioManager.STREAM_ALARM),0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_RING,audioManager.getStreamMaxVolume(AudioManager.STREAM_RING),0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_NOTIFICATION,audioManager.getStreamMaxVolume(AudioManager.STREAM_NOTIFICATION),0);
-                        audioManager.setStreamVolume(AudioManager.STREAM_VOICE_CALL,audioManager.getStreamMaxVolume(AudioManager.STREAM_VOICE_CALL),0);
-                    }
-                }
-            });
-        hSwitch=(Switch)findViewById(R.id.switch_hide);
+        mSwitch = findViewById(R.id.switch_all);
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            AudioManager audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isMuted) {
+                audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+                if (!VolumeControler.setAllVolumeMuted(audioManager, isMuted))
+                    buttonView.setChecked(!isMuted);      // 设置音量失败就恢复按钮状态
+            }
+        });
+
+        hSwitch = findViewById(R.id.switch_hide);
         hSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked)
-                {
-                        showLauncherIcon(false);
-                }else{
-                        showLauncherIcon(true);
-                }
+                showLauncherIcon(!isChecked);
             }
         });
     }
+
     public void showLauncherIcon(boolean isShow){
         PackageManager packageManager = this.getPackageManager();
-        int show = isShow? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
-        packageManager.setComponentEnabledSetting(getAliseComponentName(), show , PackageManager.DONT_KILL_APP);
+        int show = isShow ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+                          : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+        packageManager.setComponentEnabledSetting(getAliseComponentName(), show, PackageManager.DONT_KILL_APP);
     }
+
     private ComponentName getAliseComponentName(){
         return new ComponentName(MainActivity.this, "com.first.tile.MainActivityAlias");
         // 在AndroidManifest.xml中为MainActivity定义了一个别名为MainActivity-Alias的activity，是默认启动activity、是点击桌面图标后默认程序入口
